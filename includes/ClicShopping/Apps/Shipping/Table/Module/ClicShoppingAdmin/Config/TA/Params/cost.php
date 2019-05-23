39853b79f89974b5119742952153ce7bcab71817
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:50 -0400

    Add funding

--- includes/ClicShopping/Apps/Shipping/Table/Module/ClicShoppingAdmin/Config/TA/Params/cost.php
+++ includes/ClicShopping/Apps/Shipping/Table/Module/ClicShoppingAdmin/Config/TA/Params/cost.php
@@ -1,21 +1,23 @@
 <?php
   /**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
   namespace ClicShopping\Apps\Shipping\Table\Module\ClicShoppingAdmin\Config\TA\Params;
 
-  class cost extends \ClicShopping\Apps\Shipping\Table\Module\ClicShoppingAdmin\Config\ConfigParamAbstract {
+  class cost extends \ClicShopping\Apps\Shipping\Table\Module\ClicShoppingAdmin\Config\ConfigParamAbstract
+  {
     public $default = '25:8.50,50:5.50,10000:0.00';
     public $sort_order = 40;
 
-    protected function init() {
+    protected function init()
+    {
       $this->title = $this->app->getDef('cfg_table_cost_title');
       $this->description = $this->app->getDef('cfg_table_cost_desc');
     }
--- includes/ClicShopping/Apps/Shipping/Table/Module/ClicShoppingAdmin/Config/TA/Params/mode.php
+++ includes/ClicShopping/Apps/Shipping/Table/Module/ClicShoppingAdmin/Config/TA/Params/mode.php
@@ -1,33 +1,36 @@
 <?php
   /**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
 
   namespace ClicShopping\Apps\Shipping\Table\Module\ClicShoppingAdmin\Config\TA\Params;
 
   use ClicShopping\OM\HTML;
 
-  class mode extends \ClicShopping\Apps\Shipping\Table\Module\ClicShoppingAdmin\Config\ConfigParamAbstract {
+  class mode extends \ClicShopping\Apps\Shipping\Table\Module\ClicShoppingAdmin\Config\ConfigParamAbstract
+  {
     public $default = 'weight';
     public $sort_order = 10;
 
-    protected function init() {
-        $this->title = $this->app->getDef('cfg_table_mode_title');
-        $this->description = $this->app->getDef('cfg_table_mode_description');
+    protected function init()
+    {
+      $this->title = $this->app->getDef('cfg_table_mode_title');
+      $this->description = $this->app->getDef('cfg_table_mode_description');
     }
 
-    public function getInputField()  {
+    public function getInputField()
+    {
       $value = $this->getInputValue();
 
-      $input =  HTML::radioField($this->key, 'weight', $value, 'id="' . $this->key . '1" autocomplete="off"') . $this->app->getDef('cfg_table_mode_weight') . ' ';
-      $input .=  HTML::radioField($this->key, 'price', $value, 'id="' . $this->key . '2" autocomplete="off"') . $this->app->getDef('cfg_table_mode_price');
+      $input = HTML::radioField($this->key, 'weight', $value, 'id="' . $this->key . '1" autocomplete="off"') . $this->app->getDef('cfg_table_mode_weight') . ' ';
+      $input .= HTML::radioField($this->key, 'price', $value, 'id="' . $this->key . '2" autocomplete="off"') . $this->app->getDef('cfg_table_mode_price');
 
       return $input;
     }
